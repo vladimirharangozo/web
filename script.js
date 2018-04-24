ff424cbde99b485ffd1fff285b1e49669e79a901
vladimirharangozo <vladimir.harangozo@gmail.com>
2018-04-24 03:52:08 +0200

    new

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-//AOS https://github.com/michalsnik/aos
-AOS.init();
 
+    // Menu-toggle button
+
+    $(document).ready(function () {
+        $(".menu-icon").on("click", function () {
+            $("nav ul").toggleClass("showing");
+        });
+    });
+
+// Scrolling Effect
+
+$(window).on("scroll", function () {
+        if ($(window).scrollTop()) {
+            $('nav').addClass('black');
+        } else {
+            $('nav').removeClass('black');
+        }
+    })
 
